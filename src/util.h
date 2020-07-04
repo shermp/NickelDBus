f79b0009e9499954b374ad74414fb6470b77ba18
shermp <14854761+shermp@users.noreply.github.com>
2020-07-04 16:57:11 +1200

    Create d-bus assert macros
    This macro sends a d-bus error reply if the assertion fails.
    
    I feel dirty...

--- src/util.h
+++ src/util.h
@@ -18,5 +18,19 @@
 
 // Log a message and return 'ret' if cond == fail
 #define NDB_ASSERT(ret, cond, fmt, ...) if (!(cond)) { \
-    NDB_LOG(fmt, ##__VA_ARGS__); \
+    NDB_LOG(fmt, ##__VA_ARGS__);                       \
     return ret;}
+
+// like NDB_ASSERT, but sends an appropriate d-bus error on the bus before returning
+// Note: make sure you are including <QtDBus> and <QDBusContext> for this to work!
+#define NDB_DBUS_ASSERT(dbus_err, cond, fmt, ...) if (!(cond)) {      \
+    NDB_LOG(fmt, ##__VA_ARGS__);                                      \
+    if (calledFromDBus()) {                                           \
+        QString qstr;                                                 \
+        sendErrorReply((dbus_err), qstr.sprintf(fmt, ##__VA_ARGS__)); \
+    }                                                                 \
+    return NDB_DBUS_RETERR;                                           \
+}
+
+// Shorthand for the common USBMS assertion
+#define NDB_DBUS_USB_ASSERT(method_name) NDB_DBUS_ASSERT(QDBusError::InternalError, !ndbInUSBMS(), "not calling method %s: in usbms session", (method_name))
