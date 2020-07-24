0826834871cec05c06da6d1c642b5b872a3f97f5
shermp <14854761+shermp@users.noreply.github.com>
2020-07-24 22:43:54 +1200

    NDB_ASSERT() is no longer required

--- src/util.h
+++ src/util.h
@@ -5,11 +5,6 @@
     #define NH_VERSION "dev"
 #endif
 
-// Log a message and return 'ret' if cond == fail
-#define NDB_ASSERT(ret, cond, fmt, ...) if (!(cond)) { \
-    nh_log(fmt, ##__VA_ARGS__);                       \
-    return ret;}
-
 // like NDB_ASSERT, but sends an appropriate d-bus error on the bus before returning
 // Note: make sure you are including <QtDBus> and <QDBusContext> for this to work!
 #define NDB_DBUS_ASSERT(dbus_err, cond, fmt, ...) if (!(cond)) {      \
