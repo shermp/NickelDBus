a06acc46962f1661aa1a7549db17e246738827fb
shermp <14854761+shermp@users.noreply.github.com>
2020-08-28 19:58:42 +1200

    Write version string to file on init

--- src/ndb/nickeldbus.cc
+++ src/ndb/nickeldbus.cc
@@ -5,9 +5,23 @@
 
 #include "ndb.h"
 
+static const char ndb_ininstall_file[] = "/mnt/onboard/.adds/nickeldbus";
+static const char ndb_version_str[] = NH_VERSION;
+
 NDB *ndb;
 
 static int ndb_init() {
+    // Ensure that the user visible version file has the correct
+    // version number.
+    if (FILE *f = fopen(ndb_ininstall_file, "w")) {
+        nh_log("(init) Writing version to %s", ndb_ininstall_file);
+        fwrite(ndb_version_str, sizeof(ndb_version_str) - 1, 1, f);
+        fwrite("\n", sizeof(char), 1, f);
+        fclose(f);
+    } else {
+        nh_log("(init) Failed to open %s with error %m", ndb_ininstall_file);
+    }
+
     ndb = new NDB(nullptr);
     if (!ndb->initSucceeded) {
         delete ndb;
@@ -27,7 +41,7 @@ static struct nh_info NickelDBusInfo = {
     .name            = "NickelDBus",
     .desc            = "Observe and control Nickel over D-Bus",
     .uninstall_flag  = NULL,
-    .uninstall_xflag = "/mnt/onboard/.adds/nickeldbus"
+    .uninstall_xflag = ndb_ininstall_file
 };
 
 static struct nh_hook NickelDBusHook[] = {
